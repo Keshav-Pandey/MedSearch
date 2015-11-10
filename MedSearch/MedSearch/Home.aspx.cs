9ef6fd430dca20c6753cb42857d7e9d1b0eb12a1
Keshav Pandey <keshav25@outlook.com>
2015-11-10 20:19:46 +0530

    Bing Synonyms (Not working)

--- MedSearch/MedSearch/Home.aspx.cs
+++ MedSearch/MedSearch/Home.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using VDS.RDF;
 using VDS.RDF.Query;
 using VDS.RDF.Parsing;
+using BingSynonyms;
 
 namespace MedSearch
 {
@@ -18,6 +19,7 @@ namespace MedSearch
         string type;
         string imageURI;
         string abst;
+        SparqlResultSet results = new SparqlResultSet();
         private SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
         public medsearch(string q)
         {
@@ -34,8 +36,18 @@ namespace MedSearch
             var query = "";
             string resUri = "<http://dbpedia.org/resource/" + searchTerm + ">";
             query = "select ?out WHERE {" + resUri + " <http://dbpedia.org/ontology/abstract> ?out FILTER langMatches(lang(?out),'en')}";
-            SparqlResultSet results = endpoint.QueryWithResultSet(query);
-            if (results.Count != 0)
+            try
+            {
+                results = endpoint.QueryWithResultSet(query);
+            }
+            catch (Exception)
+            {
+
+                //Handle Issues
+                
+            }
+            //if (results.Count != 0)
+            if(!results.IsEmpty)
             {
                 SparqlResult res = results.First();
                 string s = res.ToString();
@@ -48,9 +60,17 @@ namespace MedSearch
             else
             {
                 query = "select ?out WHERE { ?out <http://dbpedia.org/property/tradename> ?use. ?use bif:contains \"" + searchTerm + "\"} ";
-                results = endpoint.QueryWithResultSet(query);
+                try
+                {
+                    results = endpoint.QueryWithResultSet(query);
+                }
+                catch (Exception)
+                {
+
+                    results = new SparqlResultSet();
+                }
                 //File.AppendAllText("E:\\newTest.txt", query + Environment.NewLine);
-                if (results.Count != 0)
+                if (!results.IsEmpty)
                 {
                     SparqlResult res = results.First();
                     string s = res.ToString();
@@ -107,6 +127,7 @@ namespace MedSearch
         public string searchResult = "The given term coudn't be found. Please try again.";
         public string searchImage = "Content/images/Medicine.jpg";
         public string imageResult = "no image";
+        public string searchSynonyms = "Did not find any";
 
         medsearch search;
         protected void Page_Load(object sender, EventArgs e)
@@ -116,25 +137,36 @@ namespace MedSearch
 
         protected void performSearch(object sender, EventArgs e)
         {
-            if (searchEntry.Text != "")
-            {
-                search = new medsearch(searchEntry.Text);
-                searchResult = search.getAbstract();
-                if (searchResult != "invalid search query")
-                {
-                    searchResponse = searchResult;
-                    imageResult = search.getImageURI();
-                    if (imageResult == "no image")
-                        searchImage = "Content/images/Medicine.jpg";
-                    else
-                        searchImage = imageResult;
-                }
-                else
-                {
-                    searchResponse = "The given term coudn't be found. Please try again.";
-                    searchImage = "Content/images/Sad.jpg";
-                }
-            }
+            findSynonyms();
+            //if (searchEntry.Text != "")
+            //{
+            //    search = new medsearch(searchEntry.Text);
+            //    searchResult = search.getAbstract();
+            //    if (searchResult != "invalid search query")
+            //    {
+            //        searchResponse = searchResult;
+            //        imageResult = search.getImageURI();
+            //        if (imageResult == "no image")
+            //            searchImage = "Content/images/Medicine.jpg";
+            //        else
+            //            searchImage = imageResult;
+            //    }
+            //    else
+            //    {
+            //        searchResponse = "The given term coudn't be found. Please try again.";
+            //        searchImage = "Content/images/Sad.jpg";
+            //    }
+            //}
+        }
+
+        protected void findSynonyms()
+        {
+            Uri syn = new Uri("https://api.datamarket.azure.com/Bing/Synonyms/v1/");
+            BingSynonymsContainer a = new BingSynonymsContainer(syn);
+            //GetSynonymsEntitySet ans = a.GetSynonyms("");
+            //ans = a.GetSynonyms("canon 400d");
+            //searchSynonyms = a.GetSynonyms("canon 600d");
+            System.Diagnostics.Debug.WriteLine(a.GetSynonyms("canon 400d"));
         }
     }
 }
